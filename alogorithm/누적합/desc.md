7d7758ef87d3cacf3b73f93c16f8696bd949bf59
youngseojeon <20wjsdudtj@gmail.com>
2024-01-10 15:07:16 +0900

    3차원 DP

--- alogorithm/누적합/desc.md
+++ alogorithm/누적합/desc.md
@@ -38,3 +38,21 @@ X X
 -2 0 0 0 0 +2 0   
 0 0 0 0 0 0 0    
 
+```python
+def prefix_sum(n,m):
+    prefixSum = [[0]*(m+1) for _ in range(n+1)]
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            prefixSum[i][j] = prefixSum[i-1][j] + prefixSum[i][j-1] - prefixSum[i-1][j-1] + arr[i-1][j-1]
+    return prefixSum
+
+n,m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+prefixSum = prefix_sum(n,m)
+i,j,x,y = map(int,input().split()) 
+
+# (i,j)칸에서 (x,y)칸까지 포함한 부분합
+partSum = prefixSum[x][y] - prefixSum[i-1][y] - prefixSum[x][j-1] + prefixSum[i-1][j-1]
+print(partSum)
+
+```
--- test.py
+++ test.py
@@ -1,9 +1,34 @@
-#10610
-N=list(input())
-N=sorted(N, reverse=True)
-if N[-1]!='0':
-    print(-1); exit()
-sum = 0
-for i in N: sum+=int(i)
-if sum%3: print(-1); exit()
-print(''.join(N))
+# 우유도시
+# 0까지 | 1까지 | 2까지
+N=int(input())
+List = []
+for _ in range(N):
+    List.append(list(map(int,input().split())))
+DP = [[[0 for _ in range(3)] for _ in range(N)] for _ in range(N)]
+
+# DP[0][0]에만 우선적으로 적용해보자.
+# init -> X 그냥 아래에서 적용
+
+
+for i in range(N):
+    for j in range(N):
+        if List[i][j]==0:
+            DP[i][j][0]=max(DP[i][j][0],1)
+        if i>0: #왼쪽 가능
+            val = List[i][j]
+            DP[i][j][val] = max(DP[i][j][val], DP[i-1][j][val])
+            if DP[i-1][j][(val-1+3)%3] > 0 :
+                DP[i][j][val] = max(DP[i][j][val], DP[i-1][j][(val-1+3)%3] +1)
+            DP[i][j][(val+1)%3] = max(DP[i][j][(val+1)%3],DP[i-1][j][(val+1)%3] )
+            DP[i][j][(val+2)%3] = max(DP[i][j][(val+2)%3],DP[i-1][j][(val+2)%3] )
+        if j>0: #위쪽 가능
+            val = List[i][j]
+            DP[i][j][val] = max(DP[i][j][val],DP[i][j-1][val])
+            if DP[i][j-1][(val-1+3) % 3] > 0:
+                DP[i][j][val] = max(DP[i][j][val], DP[i][j - 1][(val - 1 + 3) % 3] + 1)
+            DP[i][j][(val + 1) % 3] = max(DP[i][j][(val + 1) % 3], DP[i][j-1][(val + 1) % 3])
+            DP[i][j][(val + 2) % 3] = max(DP[i][j][(val + 2) % 3], DP[i][j-1][(val + 2) % 3])
+# for i in DP:
+#     print(i)
+
+print(max(DP[-1][-1]))
