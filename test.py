8a5b64345127076869369f11bfcacbf2afc6c5d4
youngseojeon <20wjsdudtj@gmail.com>
2024-01-11 10:11:43 +0900

    BFS 메모리초과

--- test.py
+++ test.py
@@ -1,34 +1,41 @@
-# 우유도시
-# 0까지 | 1까지 | 2까지
-N=int(input())
+### 메모리초과 -> 굳이 큐 안에 여러 정보를 넣지 말자. 더 최적화하는 방법 찾기 !
+
+from collections import deque
+
+# 공주님을 구해라 !
+N,M,T = map(int,input().split())
 List = []
 for _ in range(N):
     List.append(list(map(int,input().split())))
-DP = [[[0 for _ in range(3)] for _ in range(N)] for _ in range(N)]
 
-# DP[0][0]에만 우선적으로 적용해보자.
-# init -> X 그냥 아래에서 적용
+visited = [[0 for _ in range(M)] for _ in range(N)]
+#2가 있는 곳 - 그람
+q = deque()
+q.append([0,0]) #x,y
 
+dx, dy = [-1,1,0,0], [0,0,-1,1]
+visited[0][0]=1
 
-for i in range(N):
-    for j in range(N):
-        if List[i][j]==0:
-            DP[i][j][0]=max(DP[i][j][0],1)
-        if i>0: #왼쪽 가능
-            val = List[i][j]
-            DP[i][j][val] = max(DP[i][j][val], DP[i-1][j][val])
-            if DP[i-1][j][(val-1+3)%3] > 0 :
-                DP[i][j][val] = max(DP[i][j][val], DP[i-1][j][(val-1+3)%3] +1)
-            DP[i][j][(val+1)%3] = max(DP[i][j][(val+1)%3],DP[i-1][j][(val+1)%3] )
-            DP[i][j][(val+2)%3] = max(DP[i][j][(val+2)%3],DP[i-1][j][(val+2)%3] )
-        if j>0: #위쪽 가능
-            val = List[i][j]
-            DP[i][j][val] = max(DP[i][j][val],DP[i][j-1][val])
-            if DP[i][j-1][(val-1+3) % 3] > 0:
-                DP[i][j][val] = max(DP[i][j][val], DP[i][j - 1][(val - 1 + 3) % 3] + 1)
-            DP[i][j][(val + 1) % 3] = max(DP[i][j][(val + 1) % 3], DP[i][j-1][(val + 1) % 3])
-            DP[i][j][(val + 2) % 3] = max(DP[i][j][(val + 2) % 3], DP[i][j-1][(val + 2) % 3])
-# for i in DP:
-#     print(i)
+ans = float('inf')
 
-print(max(DP[-1][-1]))
+while q:
+    now_x, now_y = q.popleft()
+    for i in range(4):
+        nx, ny = now_x+dx[i], now_y+dy[i]
+        if nx<0 or nx>=N or ny<0 or ny>=M: continue
+        if visited[nx][ny]: continue
+        if List[nx][ny]==0 : #그냥 칸
+            visited[nx][ny]=visited[now_x][now_y]+1
+            q.append([nx, ny])
+        elif List[nx][ny]==2 : #그람
+            visited[nx][ny] = visited[now_x][now_y] + 1
+            ans = min(ans, visited[now_x][now_y]+1+(N-1-nx)+(M-1-ny)-1)
+            q.append([nx, ny])
+
+# print(visited)
+if visited[N-1][M-1]-1>=0:
+    ans = min(ans, visited[N-1][M-1]-1)
+if ans<=T :
+    print(ans)
+else:
+    print("Fail")
