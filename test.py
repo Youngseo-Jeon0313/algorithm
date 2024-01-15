baeeadf6feedc2e0d906c4b2b91f4c6295e4fbef
youngseojeon <20wjsdudtj@gmail.com>
2024-01-15 14:15:45 +0900

    애드훅

--- test.py
+++ test.py
@@ -1,18 +1,6 @@
-#11057 파이썬
+#31229
 N = int(input())
-ans = 0
-DP = [[0 for _ in range(10)] for _ in range(1000)]
-#init
-for i in range(1000):
-    for j in range(10):
-        if i==0:
-            DP[i][j]=1
-        if j==0:
-            DP[i][j]=1
-
-for i in range(1, 1000):
-    for j in range(1, 10):
-        DP[i][j]=DP[i-1][j]+DP[i][j-1]
-
-
-print(sum(DP[N-1])%10007)
+ans = []
+for i in range(N):
+    ans.append(2*i+1)
+print(*ans)
