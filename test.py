2173acb09fe95d89ad9f78e27cac6ca60bb8d6a7
youngseojeon <20wjsdudtj@gmail.com>
2024-01-15 17:57:16 +0900

    DP

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
-#31229
+#1309
 N = int(input())
-ans = []
-for i in range(N):
-    ans.append(2*i+1)
-print(*ans)
+dp = [[1,1,1] for _ in range(N)]
+for i in range(1,N):
+    for j in range(3):
+        if j==0:
+            dp[i][j]=(dp[i-1][0]+dp[i-1][1]+dp[i-1][2])%9901
+        elif j==1:
+            dp[i][j] = (dp[i - 1][0] +dp[i - 1][2])%9901
+        else:
+            dp[i][j] = (dp[i - 1][0] + dp[i - 1][1])%9901
+
+print(sum(dp[N-1])%9901)
