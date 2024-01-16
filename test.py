a96b9eacba873b1fa126d40c3b25a981758c5faa
youngseojeon <20wjsdudtj@gmail.com>
2024-01-16 14:22:08 +0900

    DP

--- test.py
+++ test.py
@@ -1,13 +1,35 @@
-#1309
-N = int(input())
-dp = [[1,1,1] for _ in range(N)]
-for i in range(1,N):
-    for j in range(3):
-        if j==0:
-            dp[i][j]=(dp[i-1][0]+dp[i-1][1]+dp[i-1][2])%9901
-        elif j==1:
-            dp[i][j] = (dp[i - 1][0] +dp[i - 1][2])%9901
-        else:
-            dp[i][j] = (dp[i - 1][0] + dp[i - 1][1])%9901
+#12852
 
-print(sum(dp[N-1])%9901)
+N = int(input())
+visited = [False for _ in range(N+1)]
+path = [0 for _ in range(N+1)] #index : to, value: from
+steps = [float('inf') for _ in range(N+1)]
+steps[N]=0
+
+# 덮어씌우기
+for i in range(N, 0,-1):
+    if   i%2==0 :
+        if steps[i//2]>steps[i]+1:
+            steps[i // 2] = steps[i] + 1
+            path[i//2]=i
+    if  i%3==0:
+        if steps[i//3]>steps[i]+1:
+            steps[i // 3] = steps[i] + 1
+            path[i//3]=i
+    if i>0 :
+        if steps[i-1]>steps[i]+1:
+            steps[i-1] = steps[i] + 1
+            path[i-1]=i
+
+
+ans = []
+now = 1
+while True:
+    ans.append(now)
+    if now==N:
+        break;
+    now = path[now]
+# print('step', steps)
+# print('path',path)
+print(steps[1])
+print(*ans[::-1])
