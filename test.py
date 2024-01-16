5708688525c81aa661024d76cdd75782b030eb04
youngseojeon <20wjsdudtj@gmail.com>
2024-01-16 15:32:34 +0900

    DP

--- test.py
+++ test.py
@@ -1,35 +1,27 @@
-#12852
+#2011
+'''
+경우
+- 건너뛴다.
+- 수를 넣는다.
+    - 앞 숫자랑 합친다. = 합칠 수 있는지 봐야 하고, 앞앞 상황 반영
+    - 안 합친다. = 앞 상황 반영
+22114
+'''
+dict = {str(i+1):chr(ord('A')+i) for i in range(26)}
+N=(input())
+N=' '+N
+LEN = len(N)
 
-N = int(input())
-visited = [False for _ in range(N+1)]
-path = [0 for _ in range(N+1)] #index : to, value: from
-steps = [float('inf') for _ in range(N+1)]
-steps[N]=0
+List = [0 for _ in range(LEN+1)] #건너뛴다/수를 넣는다.
 
-# 덮어씌우기
-for i in range(N, 0,-1):
-    if   i%2==0 :
-        if steps[i//2]>steps[i]+1:
-            steps[i // 2] = steps[i] + 1
-            path[i//2]=i
-    if  i%3==0:
-        if steps[i//3]>steps[i]+1:
-            steps[i // 3] = steps[i] + 1
-            path[i//3]=i
-    if i>0 :
-        if steps[i-1]>steps[i]+1:
-            steps[i-1] = steps[i] + 1
-            path[i-1]=i
+for i in range(LEN):
+    temp = 0
+    if i>0 and N[i-1:i+1] in dict.keys():
+        temp+=max(1,1*List[i-2])
+    if N[i] in dict.keys():  # 2다.
+        temp += max(1,1*List[i-1])
+    List[i]=temp
+    if i>0 and temp==0: print(0); exit()
 
 
-ans = []
-now = 1
-while True:
-    ans.append(now)
-    if now==N:
-        break;
-    now = path[now]
-# print('step', steps)
-# print('path',path)
-print(steps[1])
-print(*ans[::-1])
+print(List[LEN-1]%1000000)
