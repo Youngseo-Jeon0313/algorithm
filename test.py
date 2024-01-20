594dc956923044e5103ffe2ecf6e1377ebcd2f6f
전영서 <20wjsdudtj@gmail.com>
2024-01-20 21:49:41 +0900

    스텝 / index / 그 사이 -- for 문으로 해결하는 DP

--- test.py
+++ test.py
@@ -1,12 +1,30 @@
-# 24416
+'''
+ABCDE
+(A)(BCDE)
+(AB)(CDE)
+(ABC)(DE)
+(ABCD)(E)
 
+ANS 배열 따로 갱신
+'''
 
-def fib1(n):
-    if n == 1 or n == 2:
-        return 1;
-    else:
-        return (fib1(n - 1) + fib1(n - 2))
-
-N = int(input())
-
-print(fib1(N), (N-2))
+#11066
+T=int(input())
+for _ in range(T):
+    N=int(input())
+    List = list(map(int,input().split()))
+    DP=[[float('inf') for _ in range(N)] for _ in range(N)]
+    ANS=[[float('inf') for _ in range(N)] for _ in range(N)]
+    for i in range(N): # step
+        for j in range(N-i):
+            if i==0:
+                DP[j][j]=List[j]
+                ANS[j][j]=0
+            elif i==1: #step 1 처리
+                DP[j][j+i]=List[j]+List[j+i]
+                ANS[j][j+i]=List[j]+List[j+i]
+            else:
+                for k in range(j,j+i): #해당 index를 채우기 위해 여러 step들에서 끌어오기
+                    DP[j][j+i]=min(DP[j][j+i],DP[j][k]+DP[k+1][j+i])
+                    ANS[j][j+i]=min(ANS[j][j+i],DP[j][k]+DP[k+1][i+j]+ANS[j][k]+ANS[k+1][i+j])
+    print(ANS[0][N-1])
