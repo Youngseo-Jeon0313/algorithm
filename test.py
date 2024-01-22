3c7d184d658edf22be55bcd8c9a00738129ebde7
youngseojeon <20wjsdudtj@gmail.com>
2024-01-22 17:35:31 +0900

    기하학, 구현

--- test.py
+++ test.py
@@ -1,36 +1,13 @@
-### 마법의 엘리베이터
-def solution(storey):
-    answer = 0
-    div = 10
-    while storey > 0:
-        # print(storey)
-        if storey % div > 5:
-            # 더해
-            answer += 10 - (storey % div)
-            storey += 10 - (storey % div)
-        elif storey % div == 5: #나머지가 5다.
-            if storey > div and (storey // 10) % 10 > 4: #
-                answer += 10 - (storey % div)
-                storey += 10 - (storey % div)
-            else:
-                answer += storey % div
-                storey -= (storey % div)
-        else:
-            answer += storey % div
-            storey -= (storey % div)
+### 점찍기
 
-        storey //= div
+
+import math
+
+
+def solution(k, d):
+    answer = 0
+    for i in range(0, d + 1, k):
+        # print(i,int(math.sqrt(d**2-i**2))//k+1)
+        answer += int(math.sqrt(d ** 2 - i ** 2)) // k + 1
 
     return answer
-
-"""
-testcase
-5 -> 5
-6 -> 5
-2564 -> 15
-45 -> 9
-55 -> 10
-65 -> 9
-555 -> 14 *****
-
-"""
