f63a880866c949ef9c933c55dd308bb7b2385f34
youngseojeon <20wjsdudtj@gmail.com>
2024-01-24 17:31:45 +0900

    세용액

--- alogorithm/투포인터/desc.md
+++ alogorithm/투포인터/desc.md
@@ -16,3 +16,35 @@ while start<end:
 abba ->    
 ababa ->   
 
+### 투포인터의 핵심은 ?!
+투포인터로 푸는 문제인지를 파악하는 것 !!    
+DP 로 착각할 가능성이 크다.   
+2 또는 3이라는 숫자에 집중 !!   
+3151번
+O(N)
+
+### 세 용액 -> 0에 가까운 용액 찾기
+```python
+n = int(input())
+array = list(map(int, input().split()))
+
+array.sort()
+ans = float('inf')
+
+for i in range(n - 2):
+    start = i + 1
+    end = n - 1
+    while start < end:
+        SUM = array[i] + array[start] + array[end]
+        if abs(SUM) < ans:
+            ans = abs(SUM)
+            result = [array[i], array[start], array[end]]
+        if SUM < 0:
+            start += 1
+        elif SUM > 0:
+            end -= 1
+        else:
+            break
+
+print(*result)
+```
--- test.py
+++ test.py
@@ -1,62 +1,25 @@
-# 17609
-'''
-1. 회문이라는 가정
-- 짝수
-- 홀수
-2. 유사회문이라는 가정
-- 짝수
-    - 왼쪽 꺼에서 한 개를 제외한다.
-    - 오른쪽 꺼에서 한 개를 제외한다.
-- 홀수
-    - 왼쪽 꺼에서 한 개를 제외한다.
-    - 오른쪽 꺼에서 한 개를 제외한다.
-'''
+# 세 용액 으로 0에 가장 가까운 용액들 찾기
+import sys
 
+n = int(input())
+array = list(map(int, input().split()))
 
-T = int(input())
+array.sort()
+ans = float('inf')
 
-for _ in range(T):
-    answer = 0
-    s = input()
-
-    left = 0; right = len(s)-1
-    while left<right:
-        if s[left]==s[right]:
-            left+=1; right-=1
+for i in range(n - 2):
+    start = i + 1
+    end = n - 1
+    while start < end:
+        SUM = array[i] + array[start] + array[end]
+        if abs(SUM) < ans:
+            ans = abs(SUM)
+            result = [array[i], array[start], array[end]]
+        if SUM < 0:
+            start += 1
+        elif SUM > 0:
+            end -= 1
         else:
-            answer=1
             break
-    if answer ==0: print(0); continue
-    #유사회문이라고 가정
-    #왼쪽 꺼에서 한 개를 제외하겠다.
-    left = 0;
-    right = len(s) - 1
-    flag=0
-    while left < right:
-        #print(left, right, s[left], s[right])
-        if s[left] == s[right]:
-            left += 1; right -= 1
-        else:
-            if flag==0: left+=1; flag=1;
-            else: flag=2; break
-    if flag == 1:
-        answer = 1;
-        print(answer)
-        continue
-    #오른쪽 꺼에서 한 개를 제외하겠다.
-    left = 0;
-    right = len(s) - 1
-    flag = 0
-    while left < right:
-        #print(s[left], s[right])
-        if s[left] == s[right]:
-            left += 1;
-            right -= 1
-        else:
-            if flag == 0: right -= 1; flag = 1;
-            else:
-                flag = 2; break
-    if flag == 1: answer = 1;
-    else: answer = 2
 
-    print(answer)
+print(*result)
