25c9c674486f0e0e3e0520c39766bf227dcb7571
youngseojeon <20wjsdudtj@gmail.com>
2024-01-29 16:56:40 +0900

    stack 사용한 문제 .. 오큰수랑 비슷하게 활용하는데 감을 잘 못잡는 것 같음 while문 !!

--- test.py
+++ test.py
@@ -1,89 +1,17 @@
-#[PG] 숫자 카드 나누기
-
-
-def gcd(a, b):  # 최대공약수
-    if a == 0:
-        return b
-    else:
-        return gcd(b % a, a)
-
-
-def solution(arrayA, arrayB):
-    answer = 0
-    gcd_A = arrayA[0]
-    for i in range(len(arrayA)):
-        gcd_A = gcd(gcd_A, arrayA[i])
-    gcd_B = arrayB[0]
-    for j in range(len(arrayB)):
-        gcd_B = gcd(gcd_B, arrayB[j])
-
-    print(gcd_A, gcd_B)
-
-    flag = True
-    for a in arrayA:
-        if a % gcd_B == 0:
-            flag = False
-            break
-    if flag: answer = max(answer, gcd_B)
-
-    flag = True
-    for b in arrayB:
-        if b % gcd_A == 0:
-            flag = False
-            break
-    if flag: answer = max(answer, gcd_A)
-    return answer
-
+#택배상자
 '''
-테스트케이스
-6, 12, 18
-4, 8, 16
-
-답: 6  -> 처음부터 3이라고 생각해서 lcm을 같이 쓰게 됨
+5,4,3,2,1, | 10,9,8,7,6
+1 -> 발견했다면 여기에서 멈추서 deq를 살핀다. 라는 stack 이용 방식 !
 '''
 
-#여기에서 array원소의 최댓값은 1억이었다. 따라서 lcm을 쓰는 아래 코드는 반드시 overflow(런타임 에러) 발생
-
-'''
-각 array의 최대공약수와 최소공배수를 구하자.
-'''
-def gcd(a,b): #최대공약수
-    if a==0:
-        return b
-    else:
-        return gcd(b%a, a)
-
-def lcm(a,b): #최소공배수
-    return int((a*b)/gcd(a,b))
-
-def div(num):
-    divlist = []
-    for i in range(1,int(num**(1/2)) + 1):
-        if num%i==0:
-            divlist.append(i)
-            divlist.append(num//i)
-    return divlist
-
-def solution(arrayA, arrayB):
-    answer = 0
-    lcm_A = arrayA[0]
-    gcd_A = arrayA[0]
-    for i in range(len(arrayA)):
-        lcm_A = lcm(lcm_A, arrayA[i])
-        gcd_A = gcd(gcd_A, arrayA[i])
-    lcm_B = arrayB[0]
-    gcd_B = arrayB[0]
-    for j in range(len(arrayB)):
-        lcm_B = lcm(lcm_B, arrayB[j])
-        gcd_B = gcd(gcd_B, arrayB[j])
-    divlist_A = div(gcd_A)
-    divlist_B = div(gcd_B)
-    #print(lcm_A, gcd_A, lcm_B, gcd_B)
-    #print(divlist_A, divlist_B)
-    for a in divlist_A:
-        if lcm_B%a!=0:
-            if a>answer: answer = a
-    for b in divlist_B:
-        if lcm_A%b!=0:
-            if b>answer: answer = b
-    return answer
+def solution(order):
+    target = 0
+    num = 1
+    stack = []
+    while num < len(order)+1:
+        stack.append(num)
+        while stack and stack[-1]==order[target]:
+            stack.pop()
+            target +=1
+        num+=1
+    return target #order의 몇 개까지 성공했냐
