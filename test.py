24439007cd73ae9cc962ed784e367ee9ba60d686
전영서 <20wjsdudtj@gmail.com>
2024-01-31 19:58:26 +0900

    이분탐색

--- test.py
+++ test.py
@@ -8,18 +8,20 @@ for i in range(N):
 List.sort()
 left = 0; right =(List[-1]-List[0])*2
 
-while left <= right:
-    mid = (right-left)//2
-    temp = 0; pos = List[0]
+while left < right:
+    mid = (right+left)//2
+    print(left, right, mid)
+    temp = 1; pos = List[0]
     #일단 비교치를 위한 기준을 만든다.
     for i in List:
-        if i-pos>mid:
-            pos = i;
+        if i-pos>=mid:
             temp+=1
+            pos = i;
+    print(temp)        
     if temp<C: #범위를 좁힌다.
-        mid = right-1
+        right = mid
     else:
-        mid = left
+        left = mid+1
         answer = mid
 
 print(answer)
