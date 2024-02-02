1d0aab34c6c6af5b15260d2480cf5db39b5b3a99
전영서 <20wjsdudtj@gmail.com>
2024-02-02 23:38:57 +0900

    구현

--- test.py
+++ test.py
@@ -1,41 +1,28 @@
-# 백준 17179
-''' 간격에 들어맞니? 를 측정 '''
-N, M, L = map(int,input().split())
-List = []
-for _ in range(M):
-    List.append(int(input()))
-List = sorted(List)
-List=[0]+List+[L]
+# PG 롤케이크 자르기
 
-for _ in range(N):
+def solution(topping):
+    #앞에서부터 / 뒤에서부터 파악
     answer = 0
-    num = int(input())
-    left = 0; right = L;
-    while left<=right:
-        mid = (left+right)//2
-        temp = 0; cut_point = List[0]
-        for i in range(M+2):
-            if List[i]-cut_point>=mid:
-                temp+=1; cut_point = List[i]
-        print(left, right, mid, temp)
-        if temp > num:
-            left = mid + 1
-            answer = mid
-
-        else: #수가 더 크거나 같으면 위로위로 !
-            right = mid - 1
-
-    print(answer)
-
-'''
-2 5 70
-10
-20
-35
-55
-60
-1
-
-답 : 35
-
-'''
+    dict = {}
+    prefix_sum = [0 for _ in range(len(topping)+1)]
+    for i in range(len(topping)):
+        if topping[i] in dict.keys():
+            prefix_sum[i+1]=prefix_sum[i]
+        else:
+            dict[topping[i]]=1
+            prefix_sum[i+1]=prefix_sum[i]+1
+    #print(prefix_sum)
+    prefix_sum2 = [0 for _ in range(len(topping)+1)]
+    topping_new = topping[::-1]
+    dict2 = {}
+    for j in range(len(topping_new)):
+        if topping_new[j] in dict2.keys():
+            prefix_sum2[j+1]=prefix_sum2[j]
+        else:
+            dict2[topping_new[j]]=1
+            prefix_sum2[j+1]=prefix_sum2[j]+1
+    #print(prefix_sum2)
+    for k in range(len(topping)-1):
+        if prefix_sum[k]==prefix_sum2[len(topping)-k]:
+            answer+=1
+    return answer
