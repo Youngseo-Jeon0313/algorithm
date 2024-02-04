735b97f22a4fb73a343ffcf995c03c402acee22b
전영서 <20wjsdudtj@gmail.com>
2024-02-04 15:21:40 +0900

    일차원 누적합 + 슬라이딩 윈도우

--- test.py
+++ test.py
@@ -1,28 +1,20 @@
-# PG 롤케이크 자르기
+# 21921
 
-def solution(topping):
-    #앞에서부터 / 뒤에서부터 파악
-    answer = 0
-    dict = {}
-    prefix_sum = [0 for _ in range(len(topping)+1)]
-    for i in range(len(topping)):
-        if topping[i] in dict.keys():
-            prefix_sum[i+1]=prefix_sum[i]
-        else:
-            dict[topping[i]]=1
-            prefix_sum[i+1]=prefix_sum[i]+1
-    #print(prefix_sum)
-    prefix_sum2 = [0 for _ in range(len(topping)+1)]
-    topping_new = topping[::-1]
-    dict2 = {}
-    for j in range(len(topping_new)):
-        if topping_new[j] in dict2.keys():
-            prefix_sum2[j+1]=prefix_sum2[j]
-        else:
-            dict2[topping_new[j]]=1
-            prefix_sum2[j+1]=prefix_sum2[j]+1
-    #print(prefix_sum2)
-    for k in range(len(topping)-1):
-        if prefix_sum[k]==prefix_sum2[len(topping)-k]:
-            answer+=1
-    return answer
+N, X = map(int,input().split())
+visitors = list(map(int,input().split()))
+prefix_sum = [0 for _ in range(N+1)]
+for i in range(N):
+    prefix_sum[i+1]=prefix_sum[i]+visitors[i]
+ans = 0; count = 0;
+#print(prefix_sum)
+for i in range(N-X+1):
+    temp = prefix_sum[i+X]-prefix_sum[i]
+    if ans<temp:
+        ans = temp; count = 1
+    elif ans==temp:
+        count+=1
+if ans ==0:
+    print("SAD")
+else:
+    print(ans)      
+    print(count)
