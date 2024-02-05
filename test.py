d0d5d09148ae638cbaa4d8f58bcb876fb59ee36c
youngseojeon <20wjsdudtj@gmail.com>
2024-02-05 15:30:42 +0900

    DP 쉬움

--- test.py
+++ test.py
@@ -1,41 +1,15 @@
-# 백준 17179
-''' 간격에 들어맞니? 를 측정 '''
-N, M, L = map(int,input().split())
-List = []
-for _ in range(M):
-    List.append(int(input()))
-List = sorted(List)
-List=[0]+List+[L]
+# [PJ] 땅따먹기
 
-for _ in range(N):
+def solution(land):
+    #land = [[1,2,3,4]]
     answer = 0
-    num = int(input())
-    left = 0; right = L;
-    while left<=right:
-        mid = (left+right)//2
-        temp = 0; cut_point = List[0]
-        for i in range(M+2):
-            if List[i]-cut_point>=mid:
-                temp+=1; cut_point = List[i]
-        print(left, right, mid, temp)
-        if temp > num:
-            left = mid + 1
-            answer = mid
-
-        else: #수가 더 크거나 같으면 위로위로 !
-            right = mid - 1
-
-    print(answer)
-
-'''
-2 5 70
-10
-20
-35
-55
-60
-1
-
-답 : 35
-
-'''
+    DP = [[0 for _ in range(4)] for _ in range(len(land))]
+    # DP init
+    for i in range(4):
+        DP[0][i]=land[0][i]
+    for j in range(1, len(land)):
+        for k in range(4):
+            DP[j][k]=land[j][k]
+            DP[j][k]+=max(DP[j-1][(k+1)%4], DP[j-1][(k+2)%4], DP[j-1][(k+3)%4])
+    answer = (max(DP[len(land)-1]))
+    return answer
