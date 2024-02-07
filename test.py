7e9e636c1b23443885fce5bb02deca553d5b59b8
youngseojeon <20wjsdudtj@gmail.com>
2024-02-07 13:51:28 +0900

    그리디

--- test.py
+++ test.py
@@ -1,17 +1,50 @@
-# 산모양 타일링 [어려운 DP 문제 - 카카오 코테]
-def solution(n, tops):
-    MOD = 10007
-    a = [0] * (n + 1)
-    b = [0] * (n + 1)
-    a[0] = 0
-    b[0] = 1
+# n+1 카드게임 카카오
 
-    for k in range(1, n + 1):
-        if tops[k - 1]:
-            a[k] = (a[k - 1] + b[k - 1]) % MOD
-            b[k] = (2 * a[k - 1] + 3 * b[k - 1]) % MOD
-        else:
-            a[k] = (a[k - 1] + b[k - 1]) % MOD
-            b[k] = (a[k - 1] + 2 * b[k - 1]) % MOD
+def solution(coin, cards):
+    # coin = 8; cards= [1, 12, 2, 11, 3, 10, 4, 9, 5, 8, 6, 7]
+    answer = 1
+    n = len(cards)
+    visited = [0 for _ in range(n + 2)]
+    mine = cards[:(n // 3)]
+    picks = []
+    turn = 2
+    while n // 3 + turn <= n:
+        flag = 0
+        for i in mine:
+            if not visited[i] and (n + 1 - i) in mine:
+                visited[i] = 1;
+                visited[(n + 1 - i)] = 1;
+                answer += 1; flag = 1; turn += 2;
+                print(1);
+                break;
+        if flag: continue
+        for j in cards[(n // 3):(n // 3 + turn)]:
+            if not visited[j] and (n + 1 - j) in mine and coin > 0:
+                visited[j] = 1;
+                visited[n + 1 - j] = 1;
+                answer += 1; coin -= 1; flag = 2;
+                turn += 2;
+                print(2);
+                break;
+        if flag: continue
+        for k in cards[(n // 3):(n // 3 + turn)]:
+            if not visited[k] and (n + 1 - k) in cards[(n // 3):(n // 3 + turn)] and coin > 1:
+                visited[k] = 1;
+                visited[n + 1 - k] = 1;
+                answer += 1; coin -= 2; flag = 3;
+                turn += 2;
+                print(3);
+                break;
+        if flag == 0:
+            break;
 
-    return (a[n] + b[n]) % MOD
+    return answer
+
+'''
+그리디
+어렵다…
+범위 넘어가는 것 체크 필요 ! -> deq popleft 사용이 더 직관적이다.
+집중해서 풀 필요가 있다..
+테스트케이스 4까지 모두 파악해봤어야 함
+
+'''
