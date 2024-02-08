55b62f42dcb1e9177690eef1837d8a3ba76c5fbe
youngseojeon <20wjsdudtj@gmail.com>
2024-02-08 16:31:57 +0900

    스택/큐

--- test.py
+++ test.py
@@ -1,17 +1,71 @@
-# 프로그래머스 올바른 괄호
+# 스택 큐 [PG] 기능 개발
+# 예외 케이스 발견 실패로 1번 틀림
 
-def solution(s):
-    answer = True
-    stack = []
-    for i in s:
-        if i=='(':
-            stack.append('(')
-        else:
-            if stack:
-                stack.pop()
+from collections import deque
+
+def solution(progresses, speeds):
+    # progresses = [95, 90, 99, 99, 80]; speeds = [1, 1, 1, 1, 1]
+    List = []
+    answer = []
+    N = len(progresses)
+    for i in range(N):
+        List.append((100 - progresses[i]) // speeds[i] + bool((100 - progresses[i]) % speeds[i]))
+    # for k in range(N-1):
+    #     if List[k]<List[k+1]:
+    #         answer.append(k-temp)
+    #         temp = k
+    # answer.append(N-1-temp)
+    # while 문 두 개로 교체
+    deq = deque(List)
+    while deq:
+        standard = deq.popleft()
+        temp = 1
+        while deq:
+            if deq[0] <= standard:
+                deq.popleft()
+                temp += 1
             else:
-                answer = False
-    if stack:
-        answer = False
+                break;
+        answer.append(temp)
+    return answer
+
+
+'''
+    progresses = [93, 30, 55, 60, 40, 65]
+    speeds = [1, 30, 5, 10, 60, 7]
+
+    답 : [2,4]
+
+'''
+
+## 다른 풀이
+
+from collections import deque
+import math
+
+
+def solution(progresses, speeds):
+    # progresses = [95, 90, 99, 99, 80]; speeds = [1, 1, 1, 1, 1]
+    List = []
+    answer = []
+    N = len(progresses)
+    for i in range(N):
+        List.append((100 - progresses[i]) // speeds[i] + bool((100 - progresses[i]) % speeds[i]))
+
+
+    standard = 0
+    for k in range(N):
+        if List[standard] < List[k]:
+            answer.append(k - standard)
+            standard = k
+    answer.append(N - standard)
 
     return answer
+
+
+'''
+    progresses = [93, 30, 55, 60, 40, 65]
+    speeds = [1, 30, 5, 10, 60, 7]
+
+    답 : [2,4]
+'''
