f58ad6821917fe4b62f3f5f73420d17f477de00d
youngseojeon <20wjsdudtj@gmail.com>
2024-02-08 15:31:07 +0900

    스택/큐 쉬운 문제

--- test.py
+++ test.py
@@ -1,50 +1,17 @@
-# n+1 카드게임 카카오
+# 프로그래머스 올바른 괄호
 
-def solution(coin, cards):
-    # coin = 8; cards= [1, 12, 2, 11, 3, 10, 4, 9, 5, 8, 6, 7]
-    answer = 1
-    n = len(cards)
-    visited = [0 for _ in range(n + 2)]
-    mine = cards[:(n // 3)]
-    picks = []
-    turn = 2
-    while n // 3 + turn <= n:
-        flag = 0
-        for i in mine:
-            if not visited[i] and (n + 1 - i) in mine:
-                visited[i] = 1;
-                visited[(n + 1 - i)] = 1;
-                answer += 1; flag = 1; turn += 2;
-                print(1);
-                break;
-        if flag: continue
-        for j in cards[(n // 3):(n // 3 + turn)]:
-            if not visited[j] and (n + 1 - j) in mine and coin > 0:
-                visited[j] = 1;
-                visited[n + 1 - j] = 1;
-                answer += 1; coin -= 1; flag = 2;
-                turn += 2;
-                print(2);
-                break;
-        if flag: continue
-        for k in cards[(n // 3):(n // 3 + turn)]:
-            if not visited[k] and (n + 1 - k) in cards[(n // 3):(n // 3 + turn)] and coin > 1:
-                visited[k] = 1;
-                visited[n + 1 - k] = 1;
-                answer += 1; coin -= 2; flag = 3;
-                turn += 2;
-                print(3);
-                break;
-        if flag == 0:
-            break;
+def solution(s):
+    answer = True
+    stack = []
+    for i in s:
+        if i=='(':
+            stack.append('(')
+        else:
+            if stack:
+                stack.pop()
+            else:
+                answer = False
+    if stack:
+        answer = False
 
     return answer
-
-'''
-그리디
-어렵다…
-범위 넘어가는 것 체크 필요 ! -> deq popleft 사용이 더 직관적이다.
-집중해서 풀 필요가 있다..
-테스트케이스 4까지 모두 파악해봤어야 함
-
-'''
