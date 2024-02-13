022d7aa0502df597726cb55e5f111c2b44f42860
youngseojeon <20wjsdudtj@gmail.com>
2024-02-13 17:56:10 +0900

    BFS

--- test.py
+++ test.py
@@ -1,71 +1,66 @@
-# 스택 큐 [PG] 기능 개발
-# 예외 케이스 발견 실패로 1번 틀림
+# [BFS] 퍼즐 조각 채우기 - 삼성 구현 느낌의 문제
 
 from collections import deque
+'''
+회전 ㅇ / 뒤집기 x
+game_board와 table 모양을 1:1 대응시켜서 비교하자.
+'''
+dx, dy =[-1,1,0,0], [0,0,-1,1]
 
-def solution(progresses, speeds):
-    # progresses = [95, 90, 99, 99, 80]; speeds = [1, 1, 1, 1, 1]
-    List = []
-    answer = []
-    N = len(progresses)
+def puzzle_tuning(puzzle):
+    min_x = float('inf')
+    min_y = float('inf')
+    for i in puzzle:
+        min_x = min(min_x, i[0])
+        min_y = min(min_y, i[1])
+    for j in puzzle:
+        j[0]-=min_x
+        j[1]-=min_y
+    return puzzle
+
+def bfs(board, flag): # board -> puzzles
+    N = len(board)
+    visited = [[0 for _ in range(N)] for _ in range(N)]
+    puzzles = []
     for i in range(N):
-        List.append((100 - progresses[i]) // speeds[i] + bool((100 - progresses[i]) % speeds[i]))
-    # for k in range(N-1):
-    #     if List[k]<List[k+1]:
-    #         answer.append(k-temp)
-    #         temp = k
-    # answer.append(N-1-temp)
-    # while 문 두 개로 교체
-    deq = deque(List)
-    while deq:
-        standard = deq.popleft()
-        temp = 1
-        while deq:
-            if deq[0] <= standard:
-                deq.popleft()
-                temp += 1
-            else:
-                break;
-        answer.append(temp)
+        for j in range(N):
+            if not visited[i][j] and board[i][j]==flag:
+                puzzle = [[i,j]]
+                deq = deque([(i,j)])
+                while deq:
+                    y,x = deq.popleft()
+                    visited[y][x]=1
+                    for k in range(4):
+                        ny = y+dy[k]; nx = x+dx[k]
+                        if 0<=ny<N and 0<=nx<N:
+                            if not visited[ny][nx] and board[ny][nx]==flag:
+                                deq.append((ny,nx)); puzzle.append([ny,nx])
+                puzzles.append(puzzle_tuning(puzzle))
+    return puzzles
+
+def turn_90(board):
+    temp =[]
+    N=sorted(board)[-1][0]
+    for i in board:
+        temp.append([i[1],N-i[0]])
+    return temp
+
+def solution(game_board, table):
+    answer = 0
+    A = bfs(game_board, 0)
+    B = bfs(table, 1)
+    check_A = [0 for _ in range(len(A))]
+    check_B = [0 for _ in range(len(B))]
+    for a in range(len(A)):
+        for b in range(len(B)):
+            if check_A[a] or check_B[b]: continue
+            a_ = A[a]
+            b_ = B[b]
+            for c in range(4):
+                b_=turn_90(b_)
+                if sorted(a_)==sorted(b_):
+                    check_A[a]=1
+                    check_B[b]=1
+                    answer+=len(b_)
+                    break
     return answer
-
-
-'''
-    progresses = [93, 30, 55, 60, 40, 65]
-    speeds = [1, 30, 5, 10, 60, 7]
-
-    답 : [2,4]
-
-'''
-
-## 다른 풀이
-
-from collections import deque
-import math
-
-
-def solution(progresses, speeds):
-    # progresses = [95, 90, 99, 99, 80]; speeds = [1, 1, 1, 1, 1]
-    List = []
-    answer = []
-    N = len(progresses)
-    for i in range(N):
-        List.append((100 - progresses[i]) // speeds[i] + bool((100 - progresses[i]) % speeds[i]))
-
-
-    standard = 0
-    for k in range(N):
-        if List[standard] < List[k]:
-            answer.append(k - standard)
-            standard = k
-    answer.append(N - standard)
-
-    return answer
-
-
-'''
-    progresses = [93, 30, 55, 60, 40, 65]
-    speeds = [1, 30, 5, 10, 60, 7]
-
-    답 : [2,4]
-'''
