121c5725f69321217bd6c64a1d3a6ae78e783fa1
전영서 <20wjsdudtj@gmail.com>
2024-02-14 07:28:17 +0900

    bfs, 삼성 구현 문제, visited 실수

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 # [BFS] 퍼즐 조각 채우기 - 삼성 구현 느낌의 문제
-
 from collections import deque
 '''
 회전 ㅇ / 뒤집기 x
@@ -10,13 +9,13 @@ dx, dy =[-1,1,0,0], [0,0,-1,1]
 def puzzle_tuning(puzzle):
     min_x = float('inf')
     min_y = float('inf')
+    ret = []
     for i in puzzle:
         min_x = min(min_x, i[0])
         min_y = min(min_y, i[1])
     for j in puzzle:
-        j[0]-=min_x
-        j[1]-=min_y
-    return puzzle
+        ret.append([j[0]-min_x,j[1]-min_y])
+    return sorted(ret)
 
 def bfs(board, flag): # board -> puzzles
     N = len(board)
@@ -25,25 +24,27 @@ def bfs(board, flag): # board -> puzzles
     for i in range(N):
         for j in range(N):
             if not visited[i][j] and board[i][j]==flag:
-                puzzle = [[i,j]]
                 deq = deque([(i,j)])
+                puzzle=[]
                 while deq:
                     y,x = deq.popleft()
                     visited[y][x]=1
+                    puzzle.append([y,x])
                     for k in range(4):
                         ny = y+dy[k]; nx = x+dx[k]
                         if 0<=ny<N and 0<=nx<N:
                             if not visited[ny][nx] and board[ny][nx]==flag:
-                                deq.append((ny,nx)); puzzle.append([ny,nx])
+                                visited[ny][nx]=1 # 이 한 문장 때문에 ...
+                                deq.append((ny,nx)); 
                 puzzles.append(puzzle_tuning(puzzle))
     return puzzles
 
 def turn_90(board):
-    temp =[]
+    temp = []
     N=sorted(board)[-1][0]
     for i in board:
         temp.append([i[1],N-i[0]])
-    return temp
+    return puzzle_tuning(temp)
 
 def solution(game_board, table):
     answer = 0
@@ -61,6 +62,6 @@ def solution(game_board, table):
                 if sorted(a_)==sorted(b_):
                     check_A[a]=1
                     check_B[b]=1
-                    answer+=len(b_)
+                    answer+=len(a_)
                     break
     return answer
