0da94ea10e78fe87fa0987fa0f079179d05df8f5
youngseojeon <20wjsdudtj@gmail.com>
2024-02-14 16:41:10 +0900

    힙 자료구조-테스트케이스 하나 놓침

--- test.py
+++ test.py
@@ -1,67 +1,27 @@
-# [BFS] 퍼즐 조각 채우기 - 삼성 구현 느낌의 문제
-from collections import deque
-'''
-회전 ㅇ / 뒤집기 x
-game_board와 table 모양을 1:1 대응시켜서 비교하자.
-'''
-dx, dy =[-1,1,0,0], [0,0,-1,1]
+# [PG] 더 맵게 - 힙
+import heapq
 
-def puzzle_tuning(puzzle):
-    min_x = float('inf')
-    min_y = float('inf')
-    ret = []
-    for i in puzzle:
-        min_x = min(min_x, i[0])
-        min_y = min(min_y, i[1])
-    for j in puzzle:
-        ret.append([j[0]-min_x,j[1]-min_y])
-    return sorted(ret)
-
-def bfs(board, flag): # board -> puzzles
-    N = len(board)
-    visited = [[0 for _ in range(N)] for _ in range(N)]
-    puzzles = []
-    for i in range(N):
-        for j in range(N):
-            if not visited[i][j] and board[i][j]==flag:
-                deq = deque([(i,j)])
-                puzzle=[]
-                while deq:
-                    y,x = deq.popleft()
-                    visited[y][x]=1
-                    puzzle.append([y,x])
-                    for k in range(4):
-                        ny = y+dy[k]; nx = x+dx[k]
-                        if 0<=ny<N and 0<=nx<N:
-                            if not visited[ny][nx] and board[ny][nx]==flag:
-                                visited[ny][nx]=1 # 이 한 문장 때문에 ...
-                                deq.append((ny,nx)); 
-                puzzles.append(puzzle_tuning(puzzle))
-    return puzzles
-
-def turn_90(board):
-    temp = []
-    N=sorted(board)[-1][0]
-    for i in board:
-        temp.append([i[1],N-i[0]])
-    return puzzle_tuning(temp)
-
-def solution(game_board, table):
+def solution(scoville, K):
+    # scoville = [1,1]; K=2
+    # scoville = [2,2,2,1,1,8,10,12]; K=1000 #-1
+    N = len(scoville)
+    heapq.heapify(scoville)
+    # -1인 경우부터 없애자
     answer = 0
-    A = bfs(game_board, 0)
-    B = bfs(table, 1)
-    check_A = [0 for _ in range(len(A))]
-    check_B = [0 for _ in range(len(B))]
-    for a in range(len(A)):
-        for b in range(len(B)):
-            if check_A[a] or check_B[b]: continue
-            a_ = A[a]
-            b_ = B[b]
-            for c in range(4):
-                b_=turn_90(b_)
-                if sorted(a_)==sorted(b_):
-                    check_A[a]=1
-                    check_B[b]=1
-                    answer+=len(a_)
-                    break
+    last_check = 0
+    while len(scoville) > 1:
+        min_1 = heapq.heappop(scoville)
+        if min_1 >= K:
+            heapq.heappush(scoville, min_1)
+            break
+        min_2 = heapq.heappop(scoville)
+        heapq.heappush(scoville, min_1 + min_2 * 2)
+        last_check = min_1 + min_2 * 2
+        answer += 1
+
+    if answer == N - 1:
+        if last_check >= K:  # 2개인 경우 한 번 더 !!
+            return answer
+        else:
+            return -1
     return answer
