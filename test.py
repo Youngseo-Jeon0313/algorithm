74f6c48f39923bae2a3b137a6a82899f9d5c6169
전영서 <20wjsdudtj@gmail.com>
2024-02-22 16:29:21 +0900

    BFS 그리드 월드 까다로운 문제

--- test.py
+++ test.py
@@ -1,35 +1,49 @@
-# 백준 1719
+# 프로그래머스 아이템 줍기
+from collections import deque
 
-import heapq
+dx, dy = [-1,1,0,0],[0,0,-1,1]
 
-N, M = map(int,input().split())
-adj = [[] for _ in range(N+1)]
-for _ in range(M):
-    a, b, cost= map(int,input().split())
-    adj[a].append((b,cost))
-    adj[b].append((a,cost))
+def solution(rectangle, characterX, characterY, itemX, itemY):
+    for i in rectangle:
+        for j in range(4):
+            i[j]*=2
+    characterX *=2
+    characterY *=2
+    itemX*=2
+    itemY*=2    
+    
+    max_y=0; max_x=0;
+    for i in (rectangle): #y
+        max_y = max(max_y, i[1], i[3])
+        max_x = max(max_x, i[0], i[2])
+    board = [[-1 for _ in range(max_x+1)] for _ in range(max_y+1)]
+    visited = [[0 for _ in range(max_x+1)] for _ in range(max_y+1)]
+    for i in rectangle:
+        x1, y1, x2, y2 = i
+        for y in range(y1, y2+1):
+            for x in range(x1, x2+1):
+                if x1<x<x2 and y1<y<y2:
+                    board[y][x]=0
+                elif board[y][x]!=0:
+                    board[y][x]=1
+                
 
-
-dist_answer = [['' for _ in range(N+1)] for _ in range(N+1)] #여기 다시 살피기
-def dijkstra(start):
-    dist = [float('inf') for _ in range(N+1)]
-    hq = []
-    heapq.heapify(hq)
-    heapq.heappush(hq, [0,start,'-']) #cost, curr_node, 가장 먼저 가야 하는 곳
-    while hq:
-        cost, curr_node, temp_ans = heapq.heappop(hq)
-        if cost>dist[curr_node]: continue # 꺼낼 때 dist 할당하는 방법
-        dist[curr_node]=cost
-        dist_answer[start][curr_node]=temp_ans
-        for next_node, next_cost in adj[curr_node]:
-            if dist[next_node]>cost+next_cost:
-                if temp_ans=='-':
-                    heapq.heappush(hq, [cost+next_cost, next_node, next_node]) # 여기 부분 주의
-                else:
-                    heapq.heappush(hq, [cost+next_cost, next_node, temp_ans])
-for i in range(1,N+1):
-    dijkstra(i)
-for i in range(1,N+1):
-    for j in range(1,N+1):
-        print(dist_answer[i][j], end=' ')
-    print()
+    q = deque([])
+    answer =[]
+    q.append([characterY, characterX,1]) #y,x,몇번째 step
+    while q:
+        curr_y,curr_x,step = q.popleft()
+        visited[curr_y][curr_x]=step
+        for i in range(4):
+            ny, nx = curr_y+dy[i], curr_x+dx[i]
+            if ny<0 or nx<0 or ny>max_y or nx>max_x:
+                continue
+            if ny==itemY and nx==itemX:
+                visited[ny][nx]=step+1
+                answer.append(step+1)
+            elif board[ny][nx]==1:
+                if not visited[ny][nx]:
+                    visited[ny][nx]=step+1
+                    q.append([ny,nx,step+1])
+    #print(answer)
+    return (min(answer)-1)//2
