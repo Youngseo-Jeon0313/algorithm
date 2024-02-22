c6a5644728f14830ede30b3aee81aaed16127960
전영서 <20wjsdudtj@gmail.com>
2024-02-22 18:34:35 +0900

    브루트포스 O(N)으로 최적화, 많은 조건 분기 - 2번틀림 git add .

--- test.py
+++ test.py
@@ -1,49 +1,32 @@
-# 프로그래머스 아이템 줍기
-from collections import deque
+import math
+#for문 + 조회
+N,Q=map(int,input().split())
+List=list(map(int,input().split()))
+arr=[0 for _ in range(10001)]
+for i in range(N):
+    arr[List[i]]+=1
 
-dx, dy = [-1,1,0,0],[0,0,-1,1]
-
-def solution(rectangle, characterX, characterY, itemX, itemY):
-    for i in rectangle:
-        for j in range(4):
-            i[j]*=2
-    characterX *=2
-    characterY *=2
-    itemX*=2
-    itemY*=2    
-    
-    max_y=0; max_x=0;
-    for i in (rectangle): #y
-        max_y = max(max_y, i[1], i[3])
-        max_x = max(max_x, i[0], i[2])
-    board = [[-1 for _ in range(max_x+1)] for _ in range(max_y+1)]
-    visited = [[0 for _ in range(max_x+1)] for _ in range(max_y+1)]
-    for i in rectangle:
-        x1, y1, x2, y2 = i
-        for y in range(y1, y2+1):
-            for x in range(x1, x2+1):
-                if x1<x<x2 and y1<y<y2:
-                    board[y][x]=0
-                elif board[y][x]!=0:
-                    board[y][x]=1
-                
-
-    q = deque([])
-    answer =[]
-    q.append([characterY, characterX,1]) #y,x,몇번째 step
-    while q:
-        curr_y,curr_x,step = q.popleft()
-        visited[curr_y][curr_x]=step
-        for i in range(4):
-            ny, nx = curr_y+dy[i], curr_x+dx[i]
-            if ny<0 or nx<0 or ny>max_y or nx>max_x:
-                continue
-            if ny==itemY and nx==itemX:
-                visited[ny][nx]=step+1
-                answer.append(step+1)
-            elif board[ny][nx]==1:
-                if not visited[ny][nx]:
-                    visited[ny][nx]=step+1
-                    q.append([ny,nx,step+1])
-    #print(answer)
-    return (min(answer)-1)//2
+for _ in range(Q):
+    a,b=map(int,input().split())
+    flag=False
+    if a==1:
+        # ZeroDivision
+        if b==0 and sum(arr)>=2 and arr[0]>0: 
+            flag=True
+        else:
+            for i in range(1,10001):
+                if b%i==0 and arr[i]>0:
+                    #제곱일 경우
+                    if i*i==b:
+                        if arr[i]>=2:
+                            flag=True
+                    else:
+                        if len(arr)>b//i and arr[b//i]>0: 
+                            flag=True
+                        
+        if flag: 
+            print(1)
+        else:
+            print(0)
+    elif a==2 :
+        arr[List[b-1]]-=1; arr[0]+=1; List[b-1]=0
