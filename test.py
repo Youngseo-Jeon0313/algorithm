181013f5507974edef6fb31b26e8a8d587e98421
전영서 <20wjsdudtj@gmail.com>
2024-02-25 13:18:32 +0900

    index 오류 주의, 백트래킹

--- test.py
+++ test.py
@@ -1,22 +1,26 @@
-N = int(input())
-stack = []
+# 1987
+R, C = map(int,input().split())
+List = []
+for _ in range(R):
+    List.append(list(input()))
+
+visited = [0 for _ in range(ord('Z')-ord('A'))]
 answer = 0
-for step in range(N):
-    L = list(map(int,input().split()))
-    if len(L)>1 and L[0]==1:
-        #일단 얘 시작해. 그리고 넣어.
-        L[2]-=1
-        if L[2]==0:
-            answer+=L[1]
-        else:
-            stack.append(L)
-    else:
-        if stack:
-            a = stack.pop()
-            a[2]-=1
-            if a[2]==0:
-                answer+=a[1]
-            else:
-                stack.append(a)
-#print(stack)
+dy,dx=[-1,1,0,0],[0,0,-1,1]
+def dfs(row,col,temp_answer):
+    #print(col,visited)
+    global answer
+    answer = max(answer, temp_answer)
+    for i in range(4):
+        ny,nx = row+dy[i], col+dx[i]
+        if 0<=ny<R and 0<=nx<C:
+            if not visited[ord(List[ny][nx])-ord('A')]:
+                visited[ord(List[ny][nx])-ord('A')]=1
+                dfs(ny,nx,temp_answer+1)
+                visited[ord(List[ny][nx])-ord('A')]=0
+    return
+
+visited[ord(List[0][0])-ord('A')]=1
+dfs(0,0,1)
+
 print(answer)
