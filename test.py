f8676c4b688b9a00929ff1d4d060ab7716447a8c
전영서 <20wjsdudtj@gmail.com>
2024-02-26 19:25:04 +0900

    구현

--- test.py
+++ test.py
@@ -1,26 +1,24 @@
-# 1987
-R, C = map(int,input().split())
-List = []
-for _ in range(R):
-    List.append(list(input()))
+from itertools import combinations as cb
 
-visited = [0 for _ in range(ord('Z')-ord('A'))]
+# 16937
+H, W = map(int,input().split())
+N = int(input())
 answer = 0
-dy,dx=[-1,1,0,0],[0,0,-1,1]
-def dfs(row,col,temp_answer):
-    #print(col,visited)
-    global answer
-    answer = max(answer, temp_answer)
-    for i in range(4):
-        ny,nx = row+dy[i], col+dx[i]
-        if 0<=ny<R and 0<=nx<C:
-            if not visited[ord(List[ny][nx])-ord('A')]:
-                visited[ord(List[ny][nx])-ord('A')]=1
-                dfs(ny,nx,temp_answer+1)
-                visited[ord(List[ny][nx])-ord('A')]=0
-    return
+List = []
+for _ in range(N):
+    List.append(list(map(int,input().split())))
 
-visited[ord(List[0][0])-ord('A')]=1
-dfs(0,0,1)
+for i in cb(List,2):
+    a=i[0]; b=i[1]
+    a_=i[0][::-1]; b_=i[1][::-1]
+    sorts = [[a[0]+b[0],max(a[1],b[1])],[max(a[0],b[0]),a[1]+b[1]],
+             [a_[0]+b[0],max(a_[1],b[1])],[max(a_[0],b[0]),a_[1]+b[1]],
+             [a[0]+b_[0],max(a[1],b_[1])],[max(a[0],b_[0]),a[1]+b_[1]],
+             [a_[0]+b_[0],max(a_[1],b_[1])],[max(a_[0],b_[0]),a_[1]+b_[1]]]
+
+    for sort in sorts:
+        row,col=sort
+        if row<=H and col<=W:
+            answer=max(answer, (a[0]*a[1]+b[0]*b[1]))
 
 print(answer)
