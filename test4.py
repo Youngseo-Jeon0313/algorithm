b8f25420fe4051d9518d59df6b51c3438f7ad476
전영서 <20wjsdudtj@gmail.com>
2024-02-26 19:26:09 +0900

    visited 배열 0,1 말고 시간초과 해결을 위해 append pop 형태로 변경해야 함

--- test4.py
+++ test4.py
@@ -1,24 +1,35 @@
-N = int(input())
-T = input()
-M = int(input())
-prefix_sum = [[0 for _ in range(N+1)] for _ in range(ord('z')-ord('a')+1)]
-for i in range(N-1, -1, -1):
-    #해당 아스키코드
-    ascii_code = ord(T[i])-ord('a')
-    #print(ascii_code)
-    for j in range(ord('z')-ord('a')+1):
-        if j==ascii_code:
-            prefix_sum[j][i]=prefix_sum[j][i+1]+1
-        else:
-            prefix_sum[j][i]=prefix_sum[j][i+1]
-answer = 0
+import sys
+input = sys.stdin.readline
 
-for _ in range(M):
-    a,b=list(input())
-    for i in range(N):
-        if T[i]==a:
-            print(prefix_sum[ord(b)-ord('a')][i])
-            answer+=(prefix_sum[ord(b)-ord('a')][i])
-print('답',answer)
+answer = float('inf')
+answer_List = []
+def back(node,a,b,c,d,cost):
+    global answer; global answer_List;
+    if cost>answer: return
+    if a>=cond[0] and b>=cond[1] and c>=cond[2] and d>=cond[3]:
+        if answer>cost:
+            answer = cost
+            answer_List = visited.copy()
+        return
+    for j in range(node,N-1):
+        print(j+1)
+        target = j+1
+        visited.append(target)
+        temp_a, temp_b, temp_c, temp_d, temp_e = List[target]
+        back(target,a+temp_a,b+temp_b,c+temp_c,d+temp_d,cost+temp_e)
+        visited.pop()
+N=int(input()) # N이 작아서 브루트포스
+visited = []
+cond = list(map(int,input().split()))
+List = []
+for i in range(N):
+    List.append(list(map(int,input().split())))
 
-        
+back(-1,0,0,0,0, 0)
+if answer==float('inf'):
+    print(-1); exit()
+print(answer)
+for index in range(N):
+    if answer_List[index]:
+        print(index+1, end =' ')
+print()
