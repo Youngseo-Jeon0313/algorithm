1a1f9e3c5212ef5e19b3fc3c9f8ba44853a637b4
전영서 <20wjsdudtj@gmail.com>
2024-02-28 13:25:39 +0900

    union find와 플로이드워셜 문제 - 최종 answer 값 조심하기

--- test3.py
+++ test3.py
@@ -1,26 +1,70 @@
-16678
+import sys
+input = sys.stdin.readline
+
+# 2610
+
 N = int(input())
-List = []
-for _ in range(N):
-    List.append(int(input()))
-List.sort()
-target = [i+1 for i in range(N)]
-answer = 0
-for i in range(N):
-    answer+=max(0,List[i]-target[i])
-print(answer)
+M = int(input())
+parent = [i for i in range(N+1)]
 
-# N = int(input())
-# honors = []
-# for _ in range(N):
-#     honors.append(int(input()))
 
-# max_honor = 1
-# action = 0
+def union(a,b):
+    a = find(a)
+    b = find(b)
+    if a<b:
+        parent[b]=a
+    else:
+        parent[a]=b
 
-# for num in sorted(honors): #정렬 먼저
-#     if num >= max_honor: 
-#         action += num - max_honor
-#         max_honor += 1
+def find(a):
+    if a == parent[a]: #여기 잊지 말 것 !!
+        return a
+    parent[a] = find(parent[a])
+    return parent[a]
 
-# print(action)
+adj = [[] for _ in range(N+1)]
+for _ in range(M):
+    a,b = map(int,input().split())
+    adj[a].append(b)
+    adj[b].append(a)
+    union(a,b)
+for i in range(N+1):
+    find(i)
+
+dict = {}
+answer = set()
+for i in range(1,N+1):
+    answer.add(parent[i])
+    if parent[i] not in dict.keys():
+        dict[parent[i]]=[i]
+    else:
+        dict[parent[i]].append(i)
+
+print(len(answer))
+ans = []
+#print(adj)
+for key, values in dict.items():
+    DP = [[float('inf') for _ in range(N+1)] for _ in range(N+1)]
+    # 여기에서 이제 그냥 빡구현
+    for i in values:
+        DP[i][i]=0
+        for j in adj[i]:
+            DP[i][j]=1
+    for k in values:
+        for i in values:
+            for j in values:
+                if DP[i][j]>DP[i][k]+DP[k][j]:
+                    DP[i][j]=DP[i][k]+DP[k][j]
+    temp_ans = ''; min_value = float('inf')
+    #print(DP)
+    for i in values:
+        temp_max = 0
+        for j in DP[i]:
+            if j!=float('inf'): 
+                temp_max = max(temp_max,j)
+        if min_value>temp_max:
+            min_value = temp_max; temp_ans = i
+    ans.append(temp_ans)
+ans.sort()
+for i in ans:
+    print(i)
