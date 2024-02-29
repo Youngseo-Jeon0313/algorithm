224d36036202e89f607a7a3c0a071a9620f46e8a
전영서 <20wjsdudtj@gmail.com>
2024-02-29 13:13:23 +0900

    정답 비율 낮은 문제

--- test.py
+++ test.py
@@ -1,37 +1,21 @@
-#1614
+# 1788
+MOD = 1000000000
 
-'''
-1 -> 8번째마다
-2 -> (6,2) 번째마다
-3 -> 4번째마다
-4 -> (2,6) 번째마다
-5 -> 8번째마다
-
-
-'''
 N = int(input())
-M = int(input())
-answer = 0 #N이 M번째 출몰하는 위치
-answer = N
-if N==1:
-    for i in range(1,M+1):
-        answer+=8
-elif N==2:
-    for i in range(1,M+1):
-        if i%2:
-            answer+=6
-        else:
-            answer+=2
-elif N==3:
-    for i in range(1,M+1):
-        answer+=4
-elif N==4:
-    for i in range(1,M+1):
-        if i%2:
-            answer+=2
-        else:
-            answer+=6
+if N==0:
+    print(0)
+elif N>0:
+    print(1)
 else:
-    for i in range(1,M+1):
-        answer+=8
-print(answer-1)
+    if N%2:
+        print(1)
+    else:
+        print(-1)
+
+DP = [0 for _ in range(1000000+1)]
+DP[1]=1
+for i in range(2,1000000+1):
+    DP[i]=(DP[i-2]+DP[i-1])%MOD
+
+abs_N = abs(N)
+print(DP[abs_N]%MOD)
--- test2.py
+++ test2.py
@@ -1,21 +1,30 @@
-# 1141
-import sys
-input = sys.stdin.readline
+# 14426
+# 순차 비교
 
+N, M = map(int,input().split())
+S = []
+for _ in range(N):
+    S.append(input())
+L = []
+for _ in range(M):
+    L.append(input())
+S.sort(); L.sort()
 
-N = int(input())
-List = []
-for i in range(N):
-    List.append(input().rstrip())
+#print(S)
 
-List = sorted(List, key = lambda x:-len(x))
-
-answerList = []
-for i in List:
-    flag=True
-    for j in answerList:
-        if j[:len(i)]==i:
-            flag=False
-    if flag: answerList.append(i)
-
-print(len(answerList))
+#print(L)
+answer=0
+index = 0
+for i in range(len(S)):
+    while index<len(L):
+        #print('check',i,index)
+        if len(L[index])<=len(S[i]):
+            #print(S[i][:len(L[index])], L[index])
+            if L[index]==S[i][:len(L[index])]:
+                answer+=1
+        
+        #사전 순으로 나보다 앞서면 break
+        if L[index]>S[i]:
+            break;
+        index+=1
+print(answer)
