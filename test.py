c05cef1ea1987fffe6def3de3078cd72da815d26
전영서 <20wjsdudtj@gmail.com>
2024-03-10 18:24:18 +0900

    그리디

--- test.py
+++ test.py
@@ -1,59 +1,26 @@
 '''
-결국 중요한 건 어떤 게 어떤 행/열로 옮겨지냐는 것
+결국 같은 패턴이 반복되다가 처음 상태가 나오게 될 것이다!
 '''
-from itertools import permutations as pm
-from copy import deepcopy
 
-N,M,K = map(int,input().split())
-Board = []
-for _ in range(N):
-    Board.append(list(map(int,input().split())))
+N = int(input())
+P = list(map(int,input().split())) 
+S = list(map(int,input().split()))# i번째 원소를 Si번째로
+target = [0,1,2]*(N//3)
 
-rotateList = []
-for _ in range(K):
-    r,c,s = map(int,input().split())
-    rotateList.append([r-1,c-1,s])
+temp_P = P.copy()
+answer = 0
+while True:
+    if temp_P==target:
+        print(answer)
+        break;
+    answer+=1
+    #처리!
+    temp_List = temp_P.copy()
+    for i in range(N):
+        temp_List[S[i]] = temp_P[i]
+    temp_P = temp_List.copy()
+    
+    if temp_P==P:
+        print(-1)
+        break
 
-
-def rotate(board,r,c,s):
-    temp_board = deepcopy(board)
-    #오른쪽으로 가는 애들
-    width = 2*s; start = 0
-    for i in range(r-s, r):
-        for j in range(c-s+start,c-s+start+width):
-            temp_board[i][j+1]=board[i][j]
-        width -= 2; start+=1;
-    #print('오른쪽',temp_board)
-    #왼쪽으로 가는 애들
-    width = 2*s; start = 0
-    for i in range(r+s,r,-1):
-        for j in range(c-s+1+start,c-s+1+start+width): 
-            temp_board[i][j-1]=board[i][j]
-        width-=2; start+=1
-    #print('왼쪽',temp_board)
-    #위로 가는 애들
-    height = 2*s; start = 0
-    for j in range(c-s,c):
-        for i in range(r-s+1+start,r-s+1+start+height):
-            temp_board[i-1][j]=board[i][j]
-        height -=2; start+=1
-    #print('위로',temp_board)
-    #아래로 가는 애들
-    height = 2*s; start = 0
-    for j in range(c+s,c,-1):
-        for i in range(r-s+start,r-s+start+height):
-            temp_board[i+1][j]=board[i][j]
-        height-=2; start+=1
-    #print('아래로',temp_board)
-    return temp_board
-answer = float('inf')
-for comb in pm(rotateList, len(rotateList)):
-    temp_ans = deepcopy(Board)
-    #print(comb)
-    for i in comb:
-        r,c,s = i
-        temp_ans = rotate(temp_ans,r,c,s)
-    #print(temp_ans)
-    for i in temp_ans:
-        answer=min(answer, sum(i))
-print(answer)
