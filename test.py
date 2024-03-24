550e17a54da12492db023f8cc80535a869aa78ae
전영서 <20wjsdudtj@gmail.com>
2024-03-24 22:35:45 +0900

    쉬운 DP 문제

--- test.py
+++ test.py
@@ -1,21 +1,13 @@
-#중간에 나오는 수가 모두 0 이상 20 이하이어야 한다
-
-# 백준 5557
-
 N = int(input())
-List = list(map(int,input().split()))
-DP= [[0 for _ in range(21)] for _ in range(N+1)]
-
-for i in range(N):
-    if i==0:
-        DP[i][List[0]] = 1
-    else:
-        for j in range(21):
-            if DP[i-1][j]:
-                if 0<=j+List[i]<=20 :
-                    DP[i][j+List[i]] += DP[i-1][j]
-                if 0<=j-List[i]<=20 :
-                    DP[i][j-List[i]] += DP[i-1][j]
-
-
-print(DP[N-2][List[N-1]])
+List = []
+for _ in range(N):
+    List.append(list(map(int,input().split())))
+DP = [[0 for _ in range(N)] for _ in range(N)]
+DP[0][0]=List[0][0]
+for i in range(1,N):
+    for j in range(i+1):
+        if j>0:
+            DP[i][j]=max(DP[i-1][j-1]+List[i][j],DP[i-1][j]+List[i][j])
+        else:
+            DP[i][j]=DP[i-1][j]+List[i][j]
+print(max(DP[-1]))
