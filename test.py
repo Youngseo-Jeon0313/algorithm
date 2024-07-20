7cc9824c3db3de7bb098f5f7b843c084d57b3f71
전영서 <20wjsdudtj@gmail.com>
2024-07-20 14:14:05 +0900

    DP

--- test.py
+++ test.py
@@ -1,13 +1,3 @@
-N = int(input())
-List = []
-for _ in range(N):
-    List.append(list(map(int,input().split())))
-DP = [[0 for _ in range(N)] for _ in range(N)]
-DP[0][0]=List[0][0]
-for i in range(1,N):
-    for j in range(i+1):
-        if j>0:
-            DP[i][j]=max(DP[i-1][j-1]+List[i][j],DP[i-1][j]+List[i][j])
-        else:
-            DP[i][j]=DP[i-1][j]+List[i][j]
-print(max(DP[-1]))
+a = '$56.00'
+b = a.replace("\$|,", "").strip().astype(float)
+print(b)
